ac73c5c566db9b0ad7c1a7548170952a44433988
sihung.ho@urekamedia.vn <sihung.ho@urekamedia.vn>
2021-12-17 16:11:33 +0700

    Up src

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 5; $i++) {
+            DB::table('users')->insert([
+                'name' => 'user' . $i,
+                'email' => 'user' . $i . '@gmail.com',
+                'password' => bcrypt('123'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+        }
+    }
+}
